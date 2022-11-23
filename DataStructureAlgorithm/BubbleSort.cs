9ec47739e269f07f5d04b4c46f4f8dbc9fb6fa72
Divya-Bodake <divyabodake25@gmail.com>
2022-11-23 19:45:40 +0530

    [Divya]Add:BubbleSort

---
+++ DataStructureAlgorithm/BubbleSort.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructureAlgorithm
+{
+    public class BubbleSort
+    {
+        public void Sort(int[] binaryarr)
+        {
+            for (int i = 0; i < binaryarr.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (binaryarr[i].CompareTo(binaryarr[j]) < 0)
+                    {
+                        int temp = binaryarr[j];
+                        binaryarr[j] = binaryarr[i];
+                        binaryarr[i] = temp;
+                    }
+                }
+            }
+            foreach (var data in binaryarr)
+            {
+                Console.WriteLine(data);
+            }
+        }
+    }
+}
--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -7,7 +7,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("select option\n 1.Binary Search \n 2.InsertionSort");
+                Console.WriteLine("select option\n 1.Binary Search \n 2.InsertionSort\n 3.BubbleSort");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -21,6 +21,12 @@
                         int[] arr = { 1,5,7,4,8,4,8,3 };
                         InsertionSort insertionSort = new InsertionSort();
                         insertionSort.Sort(arr);
+                        break;
+
+                    case 3:
+                        BubbleSort bubbleSort = new BubbleSort();
+                        int[] binaryarr = {5, 2, 1, 4, 3};
+                        bubbleSort.Sort(binaryarr);
                         break;
                 }
             }
