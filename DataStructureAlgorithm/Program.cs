369f86c9952de6c69ec58516aba44cd7337100f6
Divya-Bodake <divyabodake25@gmail.com>
2022-11-23 19:10:15 +0530

    [Divya]Add:Binary Search the word from the binarylist

---
+++ DataStructureAlgorithm/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace DataStructureAlgorithm
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("select option\n 1.Binary Search");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        BinarySearch binarySearch = new BinarySearch();
+                        binarySearch.ReadTextFile(@"C:\\Bridgelabz\\Assignments\\Day16-17\\AlgorithmPrograms\\DataStructureAlgorithm\\BinarySearch.txt");
+                        binarySearch.BinarySearchOperation("Hello, Welcome, To, Coding");
+                        break;
+                }
+            }
+        }
+    }
+}
