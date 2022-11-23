47aa9a47a3fcca481e04bbf2df68de17c8d4b4e0
Divya-Bodake <divyabodake25@gmail.com>
2022-11-23 19:23:58 +0530

    [Divya]Add:Insertion Sort

--- DataStructureAlgorithm/Program.cs
+++ DataStructureAlgorithm/Program.cs
@@ -7,7 +7,7 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("select option\n 1.Binary Search");
+                Console.WriteLine("select option\n 1.Binary Search \n 2.InsertionSort");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -15,6 +15,12 @@
                         BinarySearch binarySearch = new BinarySearch();
                         binarySearch.ReadTextFile(@"C:\\Bridgelabz\\Assignments\\Day16-17\\AlgorithmPrograms\\DataStructureAlgorithm\\BinarySearch.txt");
                         binarySearch.BinarySearchOperation("Hello, Welcome, To, Coding");
+                        break;
+
+                        case 2:
+                        int[] arr = { 1,5,7,4,8,4,8,3 };
+                        InsertionSort insertionSort = new InsertionSort();
+                        insertionSort.Sort(arr);
                         break;
                 }
             }
